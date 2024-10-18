b39755a37a14242dcdebf22dd746f4b2cf92af69
Leo0w0 <leonardo.lopez001@outlook.com>
2024-10-18 15:29:17 -0600

    fin del ejercicio 1

--- Lab2P1_LeonardoLopez/src/lab2p1/Lab2P1.java
+++ Lab2P1_LeonardoLopez/src/lab2p1/Lab2P1.java
@@ -17,7 +17,6 @@ public class Lab2P1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char respuesta = 's';
-        
         while (respuesta == 's' || respuesta == 'S') {
             System.out.println ("Menu: ");
             System.out.println ("opcion 1: numeros pares e impares");
@@ -31,25 +30,29 @@ public class Lab2P1 {
                 int numero = scanner.nextInt();
 
                 if (numero > 31.27){
-                    int cantidad_digitos = 0;
-                    int pares = 0;
-                    int impares = 0;
-                    while (numero> 0){
-                        int digito = numero % 10;
-                        cantidad_digitos += 1;
-                            if (digito % 2 == 0){
-                                pares += 1;
-                            }
-                            else {
-                            impares += 1;
-                            }
-                    }
-                    System.out.println("Número ingresado: "+numero);
-                    System.out.println("Cantidad de dígitos: "+cantidad_digitos);
-                    System.out.println("Cantidad de dígitos pares: "+pares);
-                    System.out.println("Cantidad de dígitos impares: "+impares);
-                }    
-                else {
+        int cantidadDigitos = 0;
+        int pares = 0;
+        int impares = 0;
+        int tempNumero = numero;
+
+        while (tempNumero > 0) {
+            int digito = tempNumero % 10;
+            cantidadDigitos++;
+            if (digito % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+            tempNumero = tempNumero / 10;
+        }
+
+        System.out.println("Número ingresado: " + numero);
+        System.out.println("Cantidad de dígitos: " + cantidadDigitos);
+        System.out.println("Cantidad de dígitos pares: " + pares);
+        System.out.println("Cantidad de dígitos impares: " + impares);
+                } 
+            }
+            else {
                     System.out.print ("el numero tiene que se mayor a la raiz cuadrada de 978");
                 }    
                     
@@ -59,4 +62,4 @@ public class Lab2P1 {
         
     }
     
-}
+
